c08858ddfc0b6ceaa27ea005faa42afd7b9908aa
wallace <exnesswallace@gmail.com>
2024-06-18 17:29:37 +0200

    script that takes in a name and prints it out

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,19 @@
+// 1-stdin.js
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log("Welcome to Holberton School, what is your name?");
+
+rl.on("line", (input) => {
+  console.log(`Your name is: ${input}`);
+  rl.close();
+});
+
+process.on('exit', () => {
+  console.log("This important software is now closing");
+});
