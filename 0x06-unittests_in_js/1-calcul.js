522c5b7b891839f4ff348133435e2edea3fbbc73
wallace <exnesswallace@gmail.com>
2024-06-21 01:20:02 +0200

    package file

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,16 @@
+const calculateNumber = (type, a, b) => {
+
+  if (type === "SUM") {
+    sum = Math.round(a) + Math.round(b)
+    return (sum);
+  } else if (type === "SUBTRACT") {
+      sub = Math.round(b) - Math.round(a)
+      return (sub);
+  } else if (type === "DIVIDE") {
+      div = Math.round(a) / Math.round(b)
+      if (Math.round(b) === 0) {
+         return ("Error");
+      }
+      return (div);
+  }
+};
