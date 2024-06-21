44c0b574a520a43ac9f32f1b124a410dfaa87047
wallace <exnesswallace@gmail.com>
2024-06-21 04:41:31 +0200

    added tests

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -2,19 +2,39 @@ const assert = require('assert');
 const calculateNumber = require('./1-calcul.js');
 
 describe('calculateNumber', () => {
-  it('should return 6 when SUM type with (1.4, 4.5)', () => {
-    assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+  it('should return 4 when SUM type with (1, 3)', () => {
+    assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
   });
 
-  it('should return -4 when SUBTRACT type with (1.4, 4.5)', () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+  it('should return 5 when SUM type with (1, 3.7)', () => {
+    assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
   });
 
-  it('should return 0.2 when DIVIDE type with (1.4, 4.5)', () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+  it('should return 5 when SUM type with (1.2, 3.7)', () => {
+    assert.strictEqual(calculateNumber('SUM', 1.2, 3.7), 5);
   });
 
-  it('should return Error when DIVIDE type with (1.4, 0)', () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+  it('should return 6 when SUM type with (1.5, 3.7)', () => {
+    assert.strictEqual(calculateNumber('SUM', 1.5, 3.7), 6);
+  });
+
+  it('should return -4 when SUBTRACT type with (-1, -3)', () => {
+    assert.strictEqual(calculateNumber('SUBTRACT', -1, -3), 2);
+  });
+
+  it('should return 2 when SUBTRACT type with (3, 1)', () => {
+    assert.strictEqual(calculateNumber('SUBTRACT', 3, 1), 2);
+  });
+
+  it('should return Error when DIVIDE type with (1.5, 0)', () => {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.5, 0), 'Error');
+  });
+
+  it('should return 0.5 when DIVIDE type with (1.5, 3)', () => {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.5, 3), 0.5);
+  });
+
+  it('should return 1 when DIVIDE type with (4, 4)', () => {
+    assert.strictEqual(calculateNumber('DIVIDE', 4, 4), 1);
   });
 });
