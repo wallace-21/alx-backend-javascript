45ce7e58fba14dffbb41cc0d9c531f407dc2cda7
wallace <exnesswallace@gmail.com>
2024-06-21 04:50:33 +0200

    added tests3

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -10,12 +10,16 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
   });
 
-  it('should return 5 when SUM type with (1.2, 3.7)', () => {
-    assert.strictEqual(calculateNumber('SUM', 1.2, 3.7), 5);
+  it('should return 0 when adding 0.1 and 0.3', () => {
+    assert.strictEqual(calculateNumber('SUM', 0.1, 0.3), 0);
   });
 
   it('should return 6 when SUM type with (1.5, 3.7)', () => {
     assert.strictEqual(calculateNumber('SUM', 1.5, 3.7), 6);
+  });
+
+  it('should return 0 when subtracting 0.1 and 0.3', () => {
+    assert.strictEqual(calculateNumber('SUBTRACT', 0.1, 0.3), 0);
   });
 
   it('should return -4 when SUBTRACT type with (-1, -3)', () => {
@@ -34,7 +38,8 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber('DIVIDE', 1.5, 3), 0.5);
   });
 
-  it('should return 1 when DIVIDE type with (4, 4)', () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 4, 4), 1);
+  it('should return `Error` when dividing 1.5 and 0', () => {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.5, 0), 'Error');
   });
+
 });
