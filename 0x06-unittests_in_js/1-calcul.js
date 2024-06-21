ae5f1ce32a1d98bd4db0266906b9bf44b138fb73
wallace <exnesswallace@gmail.com>
2024-06-21 04:55:27 +0200

    declared correctly

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,13 +1,13 @@
 const calculateNumber = (type, a, b) => {
 
   if (type === "SUM") {
-    sum = Math.round(a) + Math.round(b)
+    const sum = Math.round(a) + Math.round(b)
     return (sum);
   } else if (type === "SUBTRACT") {
-      sub = Math.round(b) - Math.round(a)
+      const sub = Math.round(b) - Math.round(a)
       return (sub);
   } else if (type === "DIVIDE") {
-      div = Math.round(a) / Math.round(b)
+      const div = Math.round(a) / Math.round(b)
       if (Math.round(b) === 0) {
          return ("Error");
       }
