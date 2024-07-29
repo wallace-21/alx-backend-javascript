a4f07fea608ae77b96f0b9b97bd35f391d904e5e
wallace <exnesswallace@gmail.com>
2024-07-29 16:41:29 +0200

    fixed

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,13 +5,10 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) =>
-    results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled'
-        ? result.value
-        : `${result.reason.name}: ${result.reason.message}`,
-    }))
-  );
+  ]).then((results) => results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled'
+      ? result.value
+      : `${result.reason.name}: ${result.reason.message}`,
+  })));
 }
-
