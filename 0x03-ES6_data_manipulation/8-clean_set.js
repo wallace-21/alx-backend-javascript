cd3931cdcc8d6ebae353f28c4c1e7032f267ae7b
wallace <exnesswallace@gmail.com>
2024-07-31 10:46:14 +0200

    Handling ES6 data manipulation

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+function cleanSet(set, startString) {
+  if (!startString || startString.length === 0) {
+    return '';
+  }
+
+  return Array.from(set)
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
+}
+
+export default cleanSet;
+
