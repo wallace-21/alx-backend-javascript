97bfe0011a87eed7d64d86b577185782a66b801b
wallace <exnesswallace@gmail.com>
2024-07-31 10:15:35 +0200

    used a dif method

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,15 +1,12 @@
 import Car from './10-car';
 
-class EVCar extends Car {
+export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
 
   cloneCar() {
-    return new EVCar(this.brand, this.motor, this.color, this._range);
+    return new Car();
   }
 }
-
-export default EVCar;
-
